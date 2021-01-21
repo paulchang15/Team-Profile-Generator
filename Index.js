f6686d36fb9266d0390ccf69e11e78c4af4cdd6a
Paul Chang <paulchang959@gmail.com>
2021-01-20 22:56:08 -0800

    finished initial set up of files and logic

---
+++ Index.js
@@ -0,0 +1,16 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+const employees = [];
+
+// what type of employee to add first Man, intern, or just quit.  If quit, generate HTML and quit.
+
+// afterwards create Manager , Intern, Engineer
+class generatePage {
+  constructor() {
+    super();
+  }
+}
